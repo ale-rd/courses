d69d6b64f08ec006e2e3a95c11d1fbe3c5f778a5
Alejandro <Alejandro@Alejandro-PC>
2017-09-10 16:59:21 -0300

    Requerir y leer parámetro de la url.

--- spring-mvc-demo/src/main/java/com/demo/controllers/ParametersController.java
+++ spring-mvc-demo/src/main/java/com/demo/controllers/ParametersController.java
@@ -1,10 +1,12 @@
 package com.demo.controllers;
 
+import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.Random;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class ParametersController {
@@ -17,12 +19,38 @@ public class ParametersController {
     public String getAnotherQuote(Model model){
         
         System.out.println("URL Mapped successfully");
-        int randomNumber = new Random().nextInt(quotes.length);
-        String randomQuote = quotes[randomNumber];
+        String randomQuote = generateRandomQuote();
         model.addAttribute("randomQuote", randomQuote);
+        
+        return "quote";
+    }
+    
+    // "X-API-KEY=654321" requiere el header X-API-KEY con el valor específico 654321.
+    @RequestMapping(value = "/getUltraQuote", headers="X-API-KEY=654321")
+    public String getQuoteOnlyIfHeaderIsPresent(Model model){
+        model.addAttribute("randomQuote", this.generateRandomQuote());
+        return "quote";
+    }
+    @RequestMapping(value = "/getQuoteUsingParameter")
+    public String getQuoteUsingParameter(@RequestParam("userName") String userName, Model model){
+        
+        System.out.println("/getQuoteUsingParameter called");
+        
+        String randomQuote = generateRandomQuote();
+        model.addAttribute("randomQuote", randomQuote);
+        
+        System.out.println("User name is: " + userName);
+        model.addAttribute("userName", userName);
         
         return "quote";
         
     }
+
+    private String generateRandomQuote() {
+        int randomNumber = new Random().nextInt(quotes.length);
+        String randomQuote = quotes[randomNumber];
+        return randomQuote;
+    }
+    
 
 }
